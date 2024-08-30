aa3a6afd8579add9e694f1a156d1dcf3a19ae824
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2024-08-30 23:09:45 +0100

    adds src/ready/apis/inference_mobious.py and changes `MobiousDataset` label return to gray channel

--- src/ready/utils/datasets.py
+++ src/ready/utils/datasets.py
@@ -72,11 +72,13 @@ class MobiousDataset(Dataset):
     def __getitem__(self, idx):
         img_path = os.path.join(self.f_dir, "images", self.img_path[idx])
         label_path = os.path.join(self.f_dir, "masks", self.labels_path[idx])
-        image = read_image(img_path).type(torch.float) #/ 255
+        image = read_image(img_path).type(torch.float) #/ 255 #torch.Size([1, 3, 400, 640])
+        # image = np.asarray(Image.open( img_path ).convert("RGB")) #torch.Size([1, 400, 640, 3])
 
         # label = read_image(label_path).type(torch.float) #/ 255
-        label = np.asarray(Image.open( label_path ).convert("RGBA"))
-        label = torch.tensor(label, dtype=torch.long).permute(2, 0, 1).to(torch.float)
+        label = np.asarray(Image.open( label_path ).convert("L"))
+        # label = np.asarray(Image.open( label_path ).convert("RGBA"))
+        # label = torch.tensor(label, dtype=torch.long).permute(2, 0, 1).to(torch.float)
 
         # label =label.clone().detach()?
         # TO_TEST/TO_REMOVE
@@ -92,4 +94,5 @@ class MobiousDataset(Dataset):
         if self.target_transform:
             label = self.target_transform(label)
 
-        return image, label
+        return image, torch.tensor(label)
+
