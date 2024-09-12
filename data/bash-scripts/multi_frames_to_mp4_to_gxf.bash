39cd9d9b6905ed3c35a5ad12f2aad7f7297b2110
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2024-09-12 01:17:33 +0100

    refactor bash script to input paths and images to create multiple frame videos (mp4 and gxf*); and test it with holoscan-sdk api

--- data/bash-scripts/multi_frames_to_mp4_to_gxf.bash
+++ data/bash-scripts/multi_frames_to_mp4_to_gxf.bash
@@ -3,40 +3,33 @@
 ### USAGE
 #cd ~/ready/data/bash-scripts
 #conda activate readyVE #(in local machine)
-#bash multi_frames_to_mp4_to_gxf.bash
+#bash multi_frames_to_mp4_to_gxf.bash $HOME/Desktop/nystagmus-tracking/ready/data/openEDS/sample-frames/val3frames/images $HOME/Desktop/nystagmus-tracking/ready/data/openEDS/videos 000160 000170 000180 png
+#bash multi_frames_to_mp4_to_gxf.bash $HOME/Desktop/nystagmus-tracking/ready/data/mobious/sample-frames/test640x400/images $HOME/Desktop/nystagmus-tracking/ready/data/mobious/videos 1_1i_Ls_1 1_1i_Lr_2 1_1i_Ll_2 jpg video_3framesx10
 
 
 ### (0) SETTING UP VARIABLES
-DATA_PATH=$HOME/Desktop/nystagmus-tracking/datasets/openEDS/openEDS/openEDS/validation/images
-VIDEO_PATH=$HOME/Desktop/nystagmus-tracking/ready/data/openEDS/videos 
+DATA_PATH=$1
+VIDEO_PATH=$2
+IMAGE_ID_1=$3 #1_1i_Ls_1 #(center)
+IMAGE_ID_2=$4 #1_1i_Lr_2 #(left)
+IMAGE_ID_3=$5 #1_1i_Ll_2 #(rigth)
+IMAGE_EXTENSION=$6 #png or jpg
+VIDEO_NAME=$7 #video_3framesx10
 ID_PATH=temp
-#cd $IMAGE_PATH
 mkdir -p ${VIDEO_PATH}/${ID_PATH}
-VIDEO_NAME=video_multiple_frames
 
 
 ##### (1.A) CREATE DUPLICATE IMAGES TO CREATE video_???_duplicated_frames_of_???
-ReplicatedFrames=5
-IMAGE_ID=014340
-i=0; while ((i++ < ${ReplicatedFrames})); do cp ${DATA_PATH}/$IMAGE_ID.png ${VIDEO_PATH}/${ID_PATH}/$IMAGE_ID"_$i."png; done
+ReplicatedFrames=10
+i=0; while ((i++ < ${ReplicatedFrames})); do cp ${DATA_PATH}/${IMAGE_ID_1}.${IMAGE_EXTENSION} ${VIDEO_PATH}/${ID_PATH}/${IMAGE_ID_1}"_$i."${IMAGE_EXTENSION}; done
 
-IMAGE_ID=006110
-i=0; while ((i++ < ${ReplicatedFrames})); do cp ${DATA_PATH}/$IMAGE_ID.png ${VIDEO_PATH}/${ID_PATH}/$IMAGE_ID"_$i."png; done
+i=0; while ((i++ < ${ReplicatedFrames})); do cp ${DATA_PATH}/${IMAGE_ID_2}.${IMAGE_EXTENSION} ${VIDEO_PATH}/${ID_PATH}/${IMAGE_ID_2}"_$i."${IMAGE_EXTENSION}; done
 
-IMAGE_ID=007000
-i=0; while ((i++ < ${ReplicatedFrames})); do cp ${DATA_PATH}/$IMAGE_ID.png ${VIDEO_PATH}/${ID_PATH}/$IMAGE_ID"_$i."png; done
-
-IMAGE_ID=012700
-i=0; while ((i++ < ${ReplicatedFrames})); do cp ${DATA_PATH}/$IMAGE_ID.png ${VIDEO_PATH}/${ID_PATH}/$IMAGE_ID"_$i."png; done
-
-IMAGE_ID=023860
-i=0; while ((i++ < ${ReplicatedFrames})); do cp ${DATA_PATH}/$IMAGE_ID.png ${VIDEO_PATH}/${ID_PATH}/$IMAGE_ID"_$i."png; done
-
-IMAGE_ID=001210
-i=0; while ((i++ < ${ReplicatedFrames})); do cp ${DATA_PATH}/$IMAGE_ID.png ${VIDEO_PATH}/${ID_PATH}/$IMAGE_ID"_$i."png; done
+i=0; while ((i++ < ${ReplicatedFrames})); do cp ${DATA_PATH}/${IMAGE_ID_3}.${IMAGE_EXTENSION} ${VIDEO_PATH}/${ID_PATH}/${IMAGE_ID_3}"_$i."${IMAGE_EXTENSION}; done
 
 
-###### (1.B) CREATE RANDOM ${NUMBER_OF_IMAGES} from data to create a path of images
+
+######TODO (1.B) CREATE RANDOM ${NUMBER_OF_IMAGES} from data to create a path of images
 #NUMBER_OF_IMAGES=30
 #shuf -n ${NUMBER_OF_IMAGES} -e ${DATA_PATH}/*.png | xargs -i mv {} ${VIDEO_PATH}/${ID_PATH}
 
@@ -45,27 +38,28 @@ i=0; while ((i++ < ${ReplicatedFrames})); do cp ${DATA_PATH}/$IMAGE_ID.png ${VID
 #### (2) Converting mp4 to gxf (Graph Execution Framework) using `ffmeg` and `convert_video_to_gxf_entities.py` in local host device
 
 
-#### (2.A) INPUT ARGUMENTS FOR MP4 to GXF
+### (2.A) INPUT ARGUMENTS FOR MP4 to GXF
 #VIDEOPATH=$1
 #FRAMES_PATH=$2
-#IMAGE_EXTENSION=$3 # png or jpg
 NNframes=30
 #FRAMEFILENAMEID=$5
 WIDTH=640
 HEIGHT=400
 CHANNELS=3
 
-## CONVERT TO VIDEO
-ffmpeg -y -framerate ${NNframes} -pattern_type glob -i "${VIDEO_PATH}/${ID_PATH}/*.png" -c:v libx264 ${VIDEO_PATH}/${VIDEO_NAME}.mp4
-#ffmpeg -i ${VIDEO_PATH}/${VIDEO_NAME}.mp4
+### CONVERT TO VIDEO
+ffmpeg -y -framerate ${NNframes} -pattern_type glob -i "${VIDEO_PATH}/${ID_PATH}/*.${IMAGE_EXTENSION}" -c:v libx264 ${VIDEO_PATH}/${VIDEO_NAME}.mp4
+echo "SUCESSFUL CONVERSION OF " ${VIDEO_PATH}/${VIDEO_NAME}.mp4
 
-#### (2.B) Converting mp4 to gxf (Graph Execution Framework) using `ffmeg` and `convert_video_to_gxf_entities.py` in local host device
+##ffmpeg -i ${VIDEO_PATH}/${VIDEO_NAME}.mp4
+
+
+###### (2.B) Converting mp4 to gxf (Graph Execution Framework) using `ffmeg` and `convert_video_to_gxf_entities.py` in local host device
 cd ../../src/ready/apis/holoscan/utils/
 bash download-holohub-utils.bash
 cd ../../../../../data/bash-scripts/
+echo $pwd
 ffmpeg -y -i ${VIDEO_PATH}/${VIDEO_NAME}.mp4 -pix_fmt rgb24 -f rawvideo pipe:1 | python ../../src/ready/apis/holoscan/utils/convert_video_to_gxf_entities.py --width ${WIDTH} --height ${HEIGHT} --channels ${CHANNELS} --framerate ${NNframes} --basename ${VIDEO_PATH}/${VIDEO_NAME}
-
 echo "COMPLETION OF " ${NNframes}_duplicated_frames_of_${FRAMEFILENAMEID}_channels${CHANNELS}
-
 rm -rf ${VIDEO_PATH}/${ID_PATH}
 
--- data/mobious/videos/README.md
+++ data/mobious/videos/README.md
@@ -2,8 +2,8 @@
 
 ## Converting image to mp4 video to `gxf_entities`
 
-### 1 Select image
-
+### Single image frame
+* `640wX400h`
 ![fig](../sample-frames/1_1i_Ll_1.jpg)
 
 ### 2 convert image to frames
@@ -17,13 +17,26 @@ bash frames_to_mp4_to_gxf.bash $HOME/Desktop/nystagmus-tracking/ready/data/mobio
 ```
 
 ### 3 video properties
-
 ```
 ffmpeg -i video_30_duplicated_frames_of_1_1i_Ll_1_channels3.mp4 
 #  Stream #0:0(und): Video: h264 (High) (avc1 / 0x31637661), yuvj420p(pc, bt470bg/unknown/unknown), 640x400 [SAR 1:1 DAR 8:5], 116 kb/s, 30 fps, 30 tbr, 15360 tbn, 60 tbc (default)
 
 ffmpeg -i video_30_duplicated_frames_of_fractal-frames-in-one_channels3.mp4
 #  Stream #0:0(und): Video: h264 (High 4:4:4 Predictive) (avc1 / 0x31637661), yuv444p, 640x400 [SAR 1:1 DAR 8:5], 201 kb/s, 30 fps, 30 tbr, 15360 tbn, 60 tbc (default)
+```
+
+
+## Multiple frames
+1. Select images
+* 1_1i_Ls_1.jpg (center)
+* 1_1i_Lr_2.jpg (left)
+* 1_1i_Ll_2.jpg (rigth)
+
+2. Run bash script
+```
+cd ~/ready/data/bash-scripts
+conda activate readyVE #(in local machine)
+bash multi_frames_to_mp4_to_gxf.bash $HOME/Desktop/nystagmus-tracking/ready/data/mobious/sample-frames/test640x400/images $HOME/Desktop/nystagmus-tracking/ready/data/mobious/videos 1_1i_Ls_1 1_1i_Lr_2 1_1i_Ll_2 jpg
 ```
 
 
