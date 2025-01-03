0feb263dde3d632563b93f6f155890cdf7e04032
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2025-01-03 17:21:47 +0000

    adds other variables in config file for src/ready/apis/convert_to_onnx_and_simplify_it.py  #27

--- scripts/models/convert_to_onnx_and_simplify_it.bash
+++ scripts/models/convert_to_onnx_and_simplify_it.bash
@@ -6,4 +6,3 @@ cd $SCRIPT_PATH/../../
 source .venv/bin/activate #To activate the virtual environment
 
 python src/ready/apis/convert_to_onnx_and_simplify_it.py -c configs/models/unet/config_convert_to_onnx_and_simplify_it.yaml
-
--- src/ready/apis/convert_to_onnx_and_simplify_it.py
+++ src/ready/apis/convert_to_onnx_and_simplify_it.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     """
     Convert pytorch to onnx model and onnx model simplification
 
-    python src/ready/apis/convert_to_onnx_and_simplify_it.py -p $MODEL_PATH -m model-5jul2024.pth
+    python src/ready/apis/convert_to_onnx_and_simplify_it.py -c config/config.yaml
     """
     parser = ArgumentParser(description="Convert models pth to ONNX and simplify it (sim.onnx)")
     parser.add_argument("-c", "--config_file", help="Config filename with path", type=str)
@@ -27,22 +27,25 @@ if __name__ == "__main__":
 
     config_file = args.config_file
     config = OmegaConf.load(config_file)
-    input_model_name = config.model.input_model_name
     MODEL_PATH = config.dataset.models_path
     FULL_MODEL_PATH = os.path.join(Path.home(), MODEL_PATH)
 
+    input_model_name = config.model.input_model_name
+    input_channel_n = config.model.input_channel_n
+    output_channel_n = config.model.output_channel_n
+    image_height = config.model.image_height
+    image_width = config.model.image_width
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # DEVICE = torch.device("cpu") #"cuda"
-    channel_n = 3  # 1
 
     model_name = input_model_name[:-4]
     models_path_input_name = FULL_MODEL_PATH + "/" + input_model_name
     models_path_output_name = FULL_MODEL_PATH + "/" + model_name + ".onnx"
 
     # model = SegNet(in_chn=1, out_chn=4, BN_momentum=0.5)
-    model = UNet(nch_in=channel_n, nch_out=4)
+    model = UNet(nch_in=input_channel_n, nch_out=output_channel_n)
     model = model.to(device)
 
     model.load_state_dict(
@@ -52,15 +55,13 @@ if __name__ == "__main__":
     model = model.eval().to(device)
 
     batch_size = 1  # just a random number
-    dummy_input = torch.randn((batch_size, channel_n, 400, 640)).to(device)
-    # input size of data to the model [batch, channel, height, width]
+    dummy_input = torch.randn((batch_size, input_channel_n, image_height, image_width)).to(device)
     # torch_out = model(dump_input)
 
     export_model(model, device, models_path_output_name, dummy_input)
 
     #### simplyfing model
     # https://github.com/daquexian/onnx-simplifier?tab=readme-ov-file
-    #
     model_path_with_model = FULL_MODEL_PATH + "/" + model_name + ".onnx"
     model_path_with_simmodel = FULL_MODEL_PATH + "/" + model_name + "-sim.onnx"
 
