6098f1e5a961cd846ace591bfebfc6e7fd27a0bd
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2025-01-03 08:52:07 +0000

    reorganise config files in their respective path #27

--- scripts/models/inference_unet_with_mobious.bash
+++ scripts/models/inference_unet_with_mobious.bash
@@ -5,5 +5,4 @@ SCRIPT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_PATH/../../
 source .venv/bin/activate #To activate the virtual environment
 
-python src/ready/apis/inference_mobious.py -c configs/config_inference_unet_with_mobious.yaml
-
+python src/ready/apis/inference_mobious.py -c configs/models/unet/config_inference_unet_with_mobious.yaml
--- scripts/models/train_unet_with_mobious.bash
+++ scripts/models/train_unet_with_mobious.bash
@@ -5,4 +5,4 @@ SCRIPT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_PATH/../../
 source .venv/bin/activate #To activate the virtual environment
 
-python src/ready/apis/train_mobious.py -c configs/config_train_unet_with_mobious.yaml
+python src/ready/apis/train_mobious.py -c configs/models/unet/config_train_unet_with_mobious.yaml
