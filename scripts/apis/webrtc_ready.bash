fd282a1a6daf23d8906918c1d9c4ad9c01ea7a84
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2025-02-08 09:12:46 +0000

    adds scaffolding for api #68

---
+++ scripts/apis/webrtc_ready.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -Eeuxo pipefail
+## USAGE
+# bash webrtc.bash logger_name.log LOCAL #PUBLIC
+
+##TODO
+# https://rsalveti.wordpress.com/2007/04/03/bash-parsing-arguments-with-getopts/
+# https://aplawrence.com/Unix/getopts.html
+# https://unix.stackexchange.com/questions/677507/how-to-check-arguments-given-to-a-bash-script-efficiently
+
+cd /workspace/volumes/ready/src/ready/apis/holoscan/webrtc_ready
+KEYS_PATH=/workspace/volumes/datasets/ready/webrtc
+
+if [[ $2 == LOCAL ]]; then
+    echo $2 LOCAL network
+    clear && python webrtc_client.py --logger_filename ${KEYS_PATH}/$1
+elif [[ $2 == PUBLIC ]]; then
+    echo $2 PUBLIC network
+    clear && python webrtc_client.py --cert-file ${KEYS_PATH}/MyCertificate.crt --key-file ${KEYS_PATH}/MyKey.key --logger_filename ${KEYS_PATH}/$1
+else
+    echo "not LOCAL nor PUBLIC"
+fi
