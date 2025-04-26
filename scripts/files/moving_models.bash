c18fb3ac3fc7a4e2ca300a61988b82f0997c66a3
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2025-04-27 00:55:47 +0100

    adding untar to scripts/files/moving_models.bash #75

--- scripts/files/moving_models.bash
+++ scripts/files/moving_models.bash
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -Ee
 ## USAGE
-## bash tarfiles.bash ccxxxxx
+## bash moving_models.bash ccxxxxx #<ADD_SERVERUSERNAME>
 
 SERVERUSERNAME="$1"
-
 
 SCRIPT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_PATH/../../
@@ -13,5 +12,11 @@ eval $(parse_yaml configs/files/config_model_pathfiles.yaml)
 
 TARMODEL=weights_${model_datetimepath}_with_augmenations_${model_trainingdataname}_trained_in_${model_trainingtime}s.tar.gz
 
-echo "Compressed file: " $TARMODEL
-scp ${SERVERUSERNAME}@cricket.rc.ucl.ac.uk:${paths_ServerDataPath}/${TARMODEL} $HOME/$paths_LocalDataPath
+scp ${SERVERUSERNAME}@cricket.rc.ucl.ac.uk:${paths_ServerDataPath}/${model_datetimepath}/${TARMODEL} $HOME/$paths_LocalDataPath
+
+cd $HOME/$paths_LocalDataPath
+mkdir -p ${model_datetimepath}
+cd ${model_datetimepath}
+tar -xvzf ../${TARMODEL} 
+
+echo untar ${TARMODEL} to $HOME/$paths_LocalDataPath
