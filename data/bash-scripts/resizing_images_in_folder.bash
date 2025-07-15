c20ea34cb8899e6582220f3a0985d791f5049332
stoppedtime24 <johnfitzgerald0310@gmail.com>
2025-07-15 11:26:46 +0100

    Added test576x320 folder

--- data/bash-scripts/resizing_images_in_folder.bash
+++ data/bash-scripts/resizing_images_in_folder.bash
@@ -21,7 +21,7 @@ extensions=("jpg" "jpeg" "png")
 
 echo "Resizing images in directory: $DIR"
 echo "Output directory: $OUTPUT_DIR"
-echo "Target size: 300x170"
+echo "Target size: 576x320"
 echo "####################"
 
 # Process each image type
@@ -33,7 +33,7 @@ for ext in "${extensions[@]}"; do
             echo "Processing: $filename"
             
             # Resize image maintaining aspect ratio, then crop/pad to exact dimensions
-            convert "$file" -resize 300x170^ -gravity center -extent 300x170 "$OUTPUT_DIR/$filename"
+            convert "$file" -resize 576x320^ -gravity center -extent 576x320 "$OUTPUT_DIR/$filename"
             
             if [ $? -eq 0 ]; then
                 echo "Successfully resized"
