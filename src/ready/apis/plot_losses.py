ca897500a03298ea3b372e10ef559e8b462b298c
stoppedtime24 <johnfitzgerald0310@gmail.com>
2025-07-29 11:31:34 +0100

    plot_losses.py now plots training and validation loss values in its config file #99

--- src/ready/apis/plot_losses.py
+++ src/ready/apis/plot_losses.py
@@ -19,42 +19,37 @@ if __name__ == "__main__":
     """
 
     parser = ArgumentParser(description="Plot losses where files are in config file")
-    parser.add_argument("-c", "--config_file", help="Config filename with path", type=str)
+    parser.add_argument("-c", "--config_file", help="Config filename with path")
     args = parser.parse_args()
-
+    
+    print(args)
     config_file = args.config_file
     config = OmegaConf.load(config_file)
     MODELS_PATH=os.path.join(Path.home(), config.dataset.models_path)
+    TRAINING_LOSS = config.losses.training_loss
+    VALIDATION_LOSS = config.losses.validation_loss
 
-    path_filename1 = os.path.join(MODELS_PATH, config.losses.loss_file1)
-    path_filename2 = os.path.join(MODELS_PATH, config.losses.loss_file2)
-    path_filename3 = os.path.join(MODELS_PATH, config.losses.loss_file3)
-    path_filename4 = os.path.join(MODELS_PATH, config.losses.loss_file4)
-    path_filename5 = os.path.join(MODELS_PATH, config.losses.loss_file5)
+    path_training_loss = os.path.join(MODELS_PATH, TRAINING_LOSS)
+    path_validation_loss = os.path.join(MODELS_PATH, VALIDATION_LOSS)
 
-    df1 = pd.read_csv(path_filename1, names=['lf1'], header=None)
-    df2 = pd.read_csv(path_filename2, names=['lf2'], header=None)
-    df3 = pd.read_csv(path_filename3, names=['lf3'], header=None)
-    df4 = pd.read_csv(path_filename4, names=['lf4'], header=None)
-    df5 = pd.read_csv(path_filename5, names=['lf5'], header=None)
+    training_loss_df = pd.read_csv(path_training_loss, names=['Training Loss'])
+    validation_loss_df = pd.read_csv(path_validation_loss, names=['Validation Loss'])
+    
+    training_loss_df['epochs'] = training_loss_df.index
+    validation_loss_df['epochs'] = validation_loss_df.index
 
-    df1['epochs'] = df1.index
-    df1['lf2'] = df2
-    df1['lf3'] = df3
-    df1['lf4'] = df4
-    df1['lf5'] = df5
-
-    logger.info(f"\n Dataframe: {df1}")
+    logger.info(f"\n Training Loss Dataframe: {training_loss_df}")
+    logger.info(f"\n Validation Loss Dataframe: {validation_loss_df}")
 
     # plt.plot(df1['epochs'], df1['lf1'], df1['epochs'], df1['lf2'], df1['epochs'], df1['lf3'], df1['epochs'], df1['lf4'], df1['epochs'], df1['lf5'], linewidth=3)
-    plt.plot(df1['epochs'], df1['lf1'], df1['epochs'], df1['lf2'], df1['epochs'], df1['lf3'], linewidth=3)
+    plt.plot(training_loss_df['epochs'], training_loss_df['Training Loss'], validation_loss_df['epochs'], validation_loss_df['Validation Loss'], linewidth=3)
     # plt.title("Losses for models trained 100epochs in a100-80gb gpu")
 
+    plt.title("Training and Validation Loss Values")
     plt.xlabel("Epochs", fontsize=18)
     plt.ylabel("Loss", fontsize=18)
     plt.tick_params(axis='both', labelsize=13)
-    # plt.legend(["naug_d1144", "waug_d1144", "waug_d0572", "waug_d0286", "waug_d0145"])
-    plt.legend(["naug_d1144", "waug_d1144", "waug_d0572"], fontsize=18)
+    plt.legend(["Training", "Validation"], fontsize=18)
     plt.tight_layout()
     plt.grid()
     plt.show()
