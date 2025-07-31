3a2f1238e8546a0988b3cc556cb0a83267218263
stoppedtime24 <johnfitzgerald0310@gmail.com>
2025-07-31 15:48:45 +0100

    plot_losses.py now supports plotting loss values from multiple models #99

--- src/ready/apis/plot_losses.py
+++ src/ready/apis/plot_losses.py
@@ -25,28 +25,38 @@ if __name__ == "__main__":
     config_file = args.config_file
     config = OmegaConf.load(config_file)
     MODELS_PATH=os.path.join(Path.home(), config.dataset.models_path)
-    TRAINING_LOSS = config.losses.training_loss
-    VALIDATION_LOSS = config.losses.validation_loss
+    TRAINING_LOSS_FILES = config.losses.training_loss
+    VALIDATION_LOSS_FILES = config.losses.validation_loss 
 
-    path_training_loss = os.path.join(MODELS_PATH, TRAINING_LOSS)
-    path_validation_loss = os.path.join(MODELS_PATH, VALIDATION_LOSS)
+    for current_training_loss_file in TRAINING_LOSS_FILES:
+        
+        path_current_training_loss = os.path.join(MODELS_PATH, current_training_loss_file)
 
-    training_loss_df = pd.read_csv(path_training_loss, names=['Training Loss'])
-    validation_loss_df = pd.read_csv(path_validation_loss, names=['Validation Loss'])
+        directory, csv_file = os.path.split(current_training_loss_file)
+        current_training_loss_df = pd.read_csv(path_current_training_loss, names=[str(current_training_loss_file)])
+
+        current_training_loss_df['epochs'] = current_training_loss_df.index
+        
+        training_loss_label = "training_loss_" + str(directory) 
+        plt.plot(current_training_loss_df['epochs'], current_training_loss_df[str(current_training_loss_file)],label=training_loss_label, linewidth=3) 
+
+    for current_validation_loss_file in VALIDATION_LOSS_FILES:
+
+        path_current_validation_loss = os.path.join(MODELS_PATH, current_validation_loss_file)
+
+        directory, csv_file = os.path.split(current_validation_loss_file)
+        current_validation_loss_df = pd.read_csv(path_current_validation_loss, names=[str(current_validation_loss_file)])
+
+        current_validation_loss_df['epochs'] = current_validation_loss_df.index
+
+        validation_loss_label = "validation_loss_" + str(directory)
+        plt.plot(current_validation_loss_df['epochs'], current_validation_loss_df[str(current_validation_loss_file)], label=validation_loss_label, linewidth=3, linestyle='dashed')
     
-    training_loss_df['epochs'] = training_loss_df.index
-    validation_loss_df['epochs'] = validation_loss_df.index
-
-    logger.info(f"\n Training Loss Dataframe: {training_loss_df}")
-    logger.info(f"\n Validation Loss Dataframe: {validation_loss_df}")
-
-    plt.plot(training_loss_df['epochs'], training_loss_df['Training Loss'], validation_loss_df['epochs'], validation_loss_df['Validation Loss'], linewidth=3) 
-
-    plt.title("Training and Validation Loss for each Epoch: 27-Jul-2025_03-44-52_NVIDIA_A100_80GB_PCI", fontsize=18)
+    plt.title("Training and Validation Loss for each Epoch", fontsize=18)
     plt.xlabel("Epochs", fontsize=18)
     plt.ylabel("Loss", fontsize=18)
-    plt.tick_params(axis='both', labelsize=13)
-    plt.legend(["Training", "Validation"], fontsize=18, framealpha=0.5)
-    plt.tight_layout()
+    plt.tick_params(axis='both', labelsize=13) 
+    plt.legend(framealpha=0.5)
     plt.grid()
+    # plt.tight_layout()
     plt.show()
