1c91db6f2c998e31ede8ac9d0e56e4e3742a19c8
Mohamed <mohamedmohamoud2341@gmail.com>
2026-07-28 11:52:47 +0100

    Added all preprocessing files plus modified training files for model benchmarking

--- src/ready/utils/datasets.py
+++ src/ready/utils/datasets.py
@@ -5,6 +5,7 @@ datasets
 import os
 import random
 
+
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
@@ -24,7 +25,8 @@ class EyeDataset(Dataset):
         self.f_dir = f_dir
 
         self.img_path = list(os.listdir(os.path.join(self.f_dir, "images")))
-        self.labels_path = [i.replace(".png", ".npy") for i in self.img_path]
+        self.labels_path = [i.replace(".jpg", ".png") for i in self.img_path]
+        #self.labels_path = self.img_path  
 
     def __len__(self):
         return len(self.img_path)
@@ -32,20 +34,21 @@ class EyeDataset(Dataset):
     def __getitem__(self, idx):
         img_path = os.path.join(self.f_dir, "images", self.img_path[idx])
         image = read_image(img_path).type(torch.float) / 255
+        if image.shape[0] == 1: # grayscale -> RGB
+            image = image.repeat(3, 1, 1)
 
         # TODO add if for grayscale or rgb input image
         # grayscale to rgb
         # https://discuss.pytorch.org/t/grayscale-to-rgb-transform/18315
         # print(f'grayscale to rgb')
-        image = torch.stack([image, image, image], 1)
-        image = torch.squeeze(image)
-        # print(f"{type(image) = }, {image.dtype = }, {image.shape = }")
+        # # print(f"{type(image) = }, {image.dtype = }, {image.shape = }")
         # type(image) = <class 'torch.Tensor'>, image.dtype = torch.float32,
         # image.shape = torch.Size([3, 400, 640])
 
-        label = np.load(os.path.join(self.f_dir, "labels", self.labels_path[idx]))
+        label_path = os.path.join(self.f_dir, "masks", self.labels_path[idx])
+        label=Image.open(label_path).convert("P")
         # print(f"{type(label) = }, {label.dtype = }, {label.shape = }")
-        label = torch.tensor(label, dtype=torch.long)  # .unsqueeze(0)
+        label = torch.tensor(np.array(label), dtype=torch.long)  # .unsqueeze(0)
         #TODO add module to DEBUG LABELS
         # import matplotlib.pyplot as plt
         # print(label.long().min(), label.long().max()) # tensor(0) tensor(3)
@@ -76,6 +79,8 @@ class EyeDataset(Dataset):
             image = self.transform(image)
         if self.target_transform:
             label = self.target_transform(label)
+        
+        label=label.squeeze(0) # from torch.Size([1, 400, 640]) to #torch.Size([400, 640])
 
         return image, label
 
@@ -196,6 +201,7 @@ class MobiousDataset(Dataset):
         random.seed(seed) # apply this seed to target transform
         torch.manual_seed(seed) # needed for torchvision 0.7
         if self.target_transform:
+            encode_mask = encode_mask.unsqueeze(0)
             encode_mask = self.target_transform(encode_mask)
 
         encode_mask=encode_mask.squeeze(0) # from torch.Size([1, 400, 640]) to #torch.Size([400, 640])
