cafdd5f8459843fc37e0c40f98b44f4dda6cfb6a
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2026-07-30 00:59:44 +0100

    install holoscan-sdk-3.6.0 using holoscan cli; updates dockerfile, bashes and also run apps; resolve typos

--- docs/holoscan/launch_dev_container.bash
+++ docs/holoscan/launch_dev_container.bash
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -Eeuxo pipefail
-# USAGE
-# cd $HOME/repositories/oocular/ready/docs/holoscan
-# bash launch_dev_container.bash
-#
 
 cd $HOME/repositories/holohub
-./holohub run-container --docker-file $HOME/repositories/oocular/ready/docs/holoscan/Dockerfile --add-volume $HOME/repositories/holoscan-sdk --add-volume $HOME/repositories/oocular/ready --add-volume $HOME/datasets
+./dev_container launch --add-volume $HOME/repositories/holoscan-sdk --add-volume $HOME/repositories/oocular/ready --add-volume $HOME/datasets
